b83837e7df12439f28127eb6f42e72282aeb6503
Marten Richter <marten.richter@tu-berlin.de>
2024-12-28 19:10:35 +0100

    Correct addview command again

--- src/data.js
+++ src/data.js
@@ -333,12 +333,12 @@ export class Container extends Sink {
       return
     }
     try {
-      const tempbuffer = new ArrayBuffer(82)
+      const tempbuffer = new ArrayBuffer(84)
       const dataview = new DataView(tempbuffer)
 
       dataview.setUint8(0, 10) // major command type, startApp is 10
 
-      dataview.setUint16(1, 82) // length  1..2
+      dataview.setUint16(1, 84) // length  1..2
       dataview.setUint8(3, 0) // reserved 3
       dataview.setFloat64(4, time) // 4..11
       // positioning
@@ -359,7 +359,7 @@ export class Container extends Sink {
         dest++
       }
       // parse uuid
-      dest = 68 // 68-81
+      dest = 68 // 68-83
       for (let i = 0; i < 36; i += 2) {
         if (appid.substr(i, 1) === '-') i++
         dataview.setUint8(dest, parseInt(appid.substr(i, 2), 16))
@@ -536,7 +536,7 @@ export class MemContainer extends Container {
         time = dataview.getFloat64(position + 8)
         break
       case 10:
-        if (position + 82 > this.storageSize) return 0 // should never happen
+        if (position + 84 > this.storageSize) return 0 // should never happen
         time = dataview.getFloat64(position + 4)
         break
       case 11:
@@ -603,7 +603,7 @@ export class MemContainer extends Container {
         objnum = dataview.getUint32(position + 4)
         break
       case 10:
-        if (position + 82 > this.storageSize) return 0 // should never happen
+        if (position + 84 > this.storageSize) return 0 // should never happen
         break
       case 11:
         if (position + 12 > this.storageSize) return 0 // should never happen
@@ -839,7 +839,7 @@ export class MemContainer extends Container {
       case 10:
         {
           // now replay the data
-          if (length < 82) {
+          if (length < 84) {
             // console.log("damaged data1",length,pos);
             return -1 // damaged data
           }
@@ -856,7 +856,7 @@ export class MemContainer extends Container {
             src++
           }
           // now the uuid
-          src = 68 // 65-82
+          src = 68 // 65-84
           let appid = ''
           for (let i = 0; i < 32; i += 2) {
             const number = dataview.getUint8(pos + src)
